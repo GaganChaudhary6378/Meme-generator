ec57033eaba2c1253c5a302d1b74936e5ed959a1
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-01-15 19:38:32 +0530

    processing

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar(){
+    return (
+        <div className="nav-el">
+            <img className="img-el" src="Troll Face.png" />
+            <h2 className="nav-title">Meme Generator</h2>
+            <p className="num-el">• React Project-5</p>
+        </div>
+    )
+}
