b546d881a091edd825d199e141065fe1b6420100
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-03 21:55:10 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Meme from "./Meme";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Meme />
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <div className="footer">
+            <h2 className="footer-text">Made with ❤️ by Gagan Chaudhary</h2>
+        </div>
+    )
+}
