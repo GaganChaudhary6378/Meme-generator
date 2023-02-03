ee43ffac255f3d42063fc4b0440c8308e9df3012
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-03 20:55:31 +0530

    memes added

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -7,12 +7,29 @@ export default function Meme(){
         randomImage:"http://i.imgflip.com/1bij.jpg"
     })
 
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    },[])
+
     function handleChange(event){
         const {name,value}=event.target
         setMeme(prevMeme => ({
             ...prevMeme,
             [name] :value
         }))
+    }
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
     }
 
     return (
@@ -38,7 +55,7 @@ export default function Meme(){
                 <button className="form-button" onClick={getMemeImage}>Get a new meme image 🖼️</button>
             </div>
             <div className="meme">
-                <img src={Meme.randomImage} className="meme--image" />
+                <img src={meme.randomImage} className="meme--image" />
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
